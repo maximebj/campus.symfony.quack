02a463476c6fa5ab121be3cf9e3e58908b0a3653
Maxime BJ <maxime@dysign.fr>
2023-05-09 16:31:20 +0200

    avoid creating sub sub quack + refacto

--- src/Services/QuackService.php
+++ src/Services/QuackService.php
@@ -8,26 +8,51 @@ use Symfony\Bundle\SecurityBundle\Security;
 
 class QuackService
 {
+  protected $quack;
+
   public function __construct( 
     protected QuackRepository $quackRepository, 
     protected Security $security
-  ) {}
+  ) {
+  }
 
   public function handleQuackForm(Quack $quack, int|null $parent = null): void
   {
+    $this->quack = $quack;
+    
     # Récupérer l'utilisateur connecté
     $user = $this->security->getUser();
     
     # Définir la date de publication et l'utilisateur automatiquement
-    $quack->setCreatedAt(new \DateTime());
-    $quack->setUser($user);
+    $this->quack->setCreatedAt(new \DateTime());
+    $this->quack->setUser($user);
 
+    # Gestion du parent
+    $this->maybeSetParent($parent);
+    
+    # Enregistrer dans la base de données et flusher
+    $this->quackRepository->save($this->quack, true);
+  }
+
+  protected function maybeSetParent($parent)
+  {
     # Ajout de l'id du parent dans le cas d'un quack de réponse
-    if ( $parent !== null ) {
-      $quack->setParent($parent);
+    if ( $parent === null ) {
+      return;
     }
 
-    # Enregistrer dans la base de données et flusher
-    $this->quackRepository->save($quack, true);
+    # On vérifie que le parent n'est pas un enfant d'un autre Quack
+    # De cette manière on empêche les réponses de niveau 3
+    # l'utilisateur pourrait changer l'id du parent dans le formulaire
+
+    # Récupérer le parent
+    $parent = $this->quackRepository->find($parent);
+
+    if( $parent->getParent() !== null ) {
+      throw new \Exception("Vous ne pouvez pas répondre à un Quack qui est déjà une réponse");
+    }
+
+    # Si tout est bon, on définit le parent
+    $this->quack->setParent($parent);
   }
 }
